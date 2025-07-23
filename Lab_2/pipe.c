e9c35d48f9d57c4cabdc9e9e1bc600301f72855d
Evan Aceves <evanm.aceves@gmail.com>
2025-07-23 15:12:55 -0700

    added skeleton code

--- Lab_2/pipe.c
+++ Lab_2/pipe.c
@@ -6,6 +6,24 @@
 
 int main(int argc, char *argv[])
 {
-	// TODO: it's all yours
-	return 0;
+	int status = 0;
+	int return_status = 0;
+
+	printf("hello from PID %d\n", (int) getpid());
+
+	int rc = fork();
+	if (rc < 0) {
+		// FORK ERROR
+		fprintf(stderr, "fork failed\n");
+		exit(1);
+	} else if (rc == 0) {
+		// CHILD
+		printf("child PID: %d\n", (int) getpid());
+		return_status = execlp("ls", "ls");
+	} else {
+		// PARENT
+		wait(&status);
+		printf("parent with PID %d of child %d\n", (int) getpid(), rc);
+	}
+	return return_status;
 }
