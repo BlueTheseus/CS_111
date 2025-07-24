8f715d6200130c200ad25eebea5d7d6e67472ff2
Evan Aceves <evanm.aceves@gmail.com>
2025-07-23 22:39:50 -0700

    Solved for one command input. Also able to link first command to first pipe

--- Lab_2/pipe.c
+++ Lab_2/pipe.c
@@ -6,48 +6,103 @@
 
 int main(int argc, char *argv[])
 {
-	int status = 0;
-	int return_status = 0;
+	int exit_status;
+	int pipefd[2];
+	int childPID;
+
+	if (argv[1] == NULL) {
+		fprintf(stderr, "%s: provide at least one command.\n", argv[0]);
+		exit(1);
+	};
 
 	printf("hello from PID %d\n", (int) getpid());
 
-	if (argv[1] == NULL) {
-		fprintf(stderr, "%s: provide at least one argument\n", argv[0]);
-	};
+	// Only one command to run
+	if (argc == 2) {
+		childPID = fork();
+		switch (childPID) {
+			case -1: // FORK ERROR
+				fprintf(stderr, "fork failed\n");
+				exit(1);
 
-	for (int i = 1; i < argc; i++) {
-		printf("\nargv[%d]: %s\n", i, argv[i]);
-		int childPID = fork();
-		if (childPID < 0) {
-			// FORK ERROR
-			fprintf(stderr, "fork failed\n");
-			exit(1);
-		} else if (childPID == 0) {
-			// CHILD
-			return_status = execlp(argv[i], argv[i], (char*) NULL);
-			fprintf(stderr, "execlp failed\n");
-			exit(return_status);
-		} else {
-			// PARENT
-			wait(&status);
-			//printf("\nall commands finished\n");
+			case 0:  // CHILD
+				exit_status = execlp(argv[1], argv[1], (char *) NULL);
+				fprintf(stderr, "execlp failed\n");
+				exit(exit_status);
+
+			default: // PARENT
+				wait(&exit_status);
+				return WEXITSTATUS(exit_status);
 		}
 	}
+
+	// Create a pipe -- 0=readable, 1=writeable
+	if (pipe(pipefd) == -1) {
+		fprintf(stderr, "error creating pipe");
+		exit(1);
+	}
+
+	// Have first program output to pipe
+	childPID = fork();
+	switch (childPID) {
+		case -1: // FORK ERROR
+			fprintf(stderr, "fork failed\n");
+			exit(1);
+
+		case 0:  // CHILD
+			if (close(pipefd[0]) == -1) {
+				fprintf(stderr, "error closing unused pipe input");
+				exit(1);
+			}
+			dup2(pipefd[1], STDOUT_FILENO); // Link stdout to write to the pipe
+			exit_status = execlp(argv[1], argv[1], (char *) NULL);
+			fprintf(stderr, "execlp failed\n");
+			exit(exit_status);
+
+		default: // PARENT
+			if (close(pipefd[1]) == -1) {
+				fprintf(stderr, "error closing unused pipe output");
+				exit(1);
+			}
+			dup2(pipefd[0], STDIN_FILENO);
+			wait(&exit_status);
+			execlp("cat", "cat", (char *) NULL);
+	}
+
+
+
+	/*for (int i = 1; i < argc; i++) {
+		printf("\nargv[%d]: %s\n", i, argv[i]);
+
+		// Create a pipe
+		if (pipe(pipefd) == -1) {
+			fprintf(stderr, "pipe error");
+			exit(1);
+		}
+
+		// Fork
+		childPID = fork();
+		switch (childPID) {
+			case -1: // FORK ERROR
+				fprintf(stderr, "fork failed\n");
+				exit(1);
+
+			case 0:  // CHILD
+				if (close(pipefd[1]) == -1) {
+					fprintf(stderr, "error closing unused write-end");
+					exit(1);
+				}
+				dup2(pipefd[]);
+				int return_status = execlp(argv[i], argv[i], (char*) NULL);
+				fprintf(stderr, "execlp failed\n");
+				exit(return_status);
+
+			default: // PARENT
+				wait(&status);
+		}
+	}*/
+
 	printf("\nall commands finished\n");
 
-/*	int rc = fork();
-	if (rc < 0) {
-		// FORK ERROR
-		fprintf(stderr, "fork failed\n");
-		exit(1);
-	} else if (rc == 0) {
-		// CHILD
-		printf("child PID: %d\n", (int) getpid());
-		return execlp("ls", "ls");
-	} else {
-		// PARENT
-		wait(&status);
-		printf("parent with PID %d of child %d\n", (int) getpid(), rc);
-	}*/
-	return return_status;
+	return 0;
 }
