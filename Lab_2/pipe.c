cf07bad88ba204f46d15e0ac01713ab9cc4b2395
Evan Aceves <evanm.aceves@gmail.com>
2025-07-23 23:25:06 -0700

    preparing abstractions for dealing with middle programs

--- Lab_2/pipe.c
+++ Lab_2/pipe.c
@@ -62,10 +62,11 @@ int main(int argc, char *argv[])
 	}
 
 	// Handle pipes for all middle programs
+	int previous_pipe_read = pipefd[0];
 	/*for (int i = 1; i < argc-1; i++) {
 
-		// Create a pipe
-		// SAVE READ
+		// Create a new pipe
+		//previous_pipe_read = pipefd[0];
 		if (pipe(pipefd) == -1) {
 			fprintf(stderr, "pipe error");
 			exit(1);
@@ -79,11 +80,12 @@ int main(int argc, char *argv[])
 				exit(1);
 
 			case 0:  // CHILD
-				if (close(pipefd[1]) == -1) {
-					fprintf(stderr, "error closing unused write-end");
+				if (close(pipefd[0]) == -1) {
+					fprintf(stderr, "error closing unused readable pipe");
 					exit(1);
 				}
-				dup2(pipefd[]);
+				dup2(previous_pipe_read, STDIN_FILENO); // Link stdin to read previous pipe
+				dup2(pipefd[1], STDOUT_FILENO); // Link stdout to write to next pipe
 				int return_status = execlp(argv[i], argv[i], (char*) NULL);
 				fprintf(stderr, "execlp failed\n");
 				exit(return_status);
@@ -105,13 +107,14 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "error closing unused writeable pipe");
 				exit(1);
 			}
-			dup2(pipefd[0], STDIN_FILENO); // Link stdin to read from the pipe
+			//dup2(pipefd[0], STDIN_FILENO); // Link stdin to read from the pipe
+			dup2(previous_pipe_read, STDIN_FILENO); // Link stdin to read from the pipe
 			exit_status = execlp(argv[argc-1], argv[argc-1], (char *) NULL);
 			fprintf(stderr, "execlp failed\n");
 			exit(exit_status);
 
 		default: // PARENT
-			if (close(pipefd[0]) == -1) {
+			if (close(previous_pipe_read) == -1) {
 				fprintf(stderr, "error closing unused readable pipe");
 				exit(1);
 			}
