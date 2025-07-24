db544ead9ec60f173d7f838e7f7f4cd3ac87f441
Evan Aceves <evanm.aceves@gmail.com>
2025-07-23 23:58:04 -0700

    cleaning comments

--- Lab_2/pipe.c
+++ Lab_2/pipe.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
 	int pipefd[2];
 	int childPID;
 
-	//printf("argc: %d\n", argc);
 	if (argv[1] == NULL) {
 		fprintf(stderr, "%s: provide at least one command.\n", argv[0]);
 		exit(1);
@@ -37,7 +36,7 @@ int main(int argc, char *argv[])
 
 	// Create a pipe -- 0=readable, 1=writeable
 	if (pipe(pipefd) == -1) {
-		fprintf(stderr, "error creating pipe");
+		fprintf(stderr, "error creating pipe\n");
 		exit(1);
 	}
 
@@ -50,7 +49,7 @@ int main(int argc, char *argv[])
 
 		case 0:  // CHILD
 			if (close(pipefd[0]) == -1) {
-				fprintf(stderr, "error closing unused readable pipe");
+				fprintf(stderr, "error closing unused readable pipe\n");
 				exit(1);
 			}
 			dup2(pipefd[1], STDOUT_FILENO); // Link stdout to write to the pipe
@@ -61,7 +60,7 @@ int main(int argc, char *argv[])
 		default: // PARENT
 			//wait(&exit_status);
 			if (close(pipefd[1]) == -1) {
-				fprintf(stderr, "error closing unused writeable pipe");
+				fprintf(stderr, "error closing unused writeable pipe\n");
 				exit(1);
 			}
 			wait(&exit_status);
@@ -70,15 +69,11 @@ int main(int argc, char *argv[])
 
 	// Handle pipes for all middle programs
 	for (int i = 2; i < argc-1; i++) {
-		printf("loop iteration %d\n", i);
-
 		// Create a new pipe
-		//previous_pipe_read = pipefd[0];
 		if (pipe(pipefd) == -1) {
-			fprintf(stderr, "pipe error");
+			fprintf(stderr, "error creating new pipe\n");
 			exit(1);
 		}
-		printf("...created new pipe\n");
 
 		// Fork
 		childPID = fork();
@@ -88,9 +83,8 @@ int main(int argc, char *argv[])
 				exit(1);
 
 			case 0:  // CHILD
-				printf("...child created\n");
 				if (close(pipefd[0]) == -1) {
-					fprintf(stderr, "error closing unused readable pipe");
+					fprintf(stderr, "error closing unused readable pipe\n");
 					exit(1);
 				}
 				dup2(previous_pipe_read, STDIN_FILENO); // Link stdin to read previous pipe
@@ -100,20 +94,18 @@ int main(int argc, char *argv[])
 				exit(exit_status);
 
 			default: // PARENT
-				printf("...parent waiting\n");
 				wait(&exit_status);
 				if (close(previous_pipe_read) == -1) {
-					fprintf(stderr, "error closing unused readable pipe");
+					fprintf(stderr, "error closing unused readable pipe\n");
 					exit(1);
 				}
 				//wait(&exit_status);
 				if (close(pipefd[1]) == -1) {
-					fprintf(stderr, "error closing unused writeable pipe");
+					fprintf(stderr, "error closing unused writeable pipe\n");
 					exit(1);
 				}
 				//wait(&exit_status);
 				previous_pipe_read = pipefd[0];
-				printf("...parent done waiting\n");
 		}
 	}
 
@@ -125,11 +117,6 @@ int main(int argc, char *argv[])
 			exit(1);
 
 		case 0:  // CHILD
-			/*if (close(pipefd[1]) == -1) {
-				fprintf(stderr, "error closing unused writeable pipe");
-				exit(1);
-			}*/
-			//dup2(pipefd[0], STDIN_FILENO); // Link stdin to read from the pipe
 			dup2(previous_pipe_read, STDIN_FILENO); // Link stdin to read from the pipe
 			exit_status = execlp(argv[argc-1], argv[argc-1], (char *) NULL);
 			fprintf(stderr, "execlp failed\n");
@@ -137,13 +124,9 @@ int main(int argc, char *argv[])
 
 		default: // PARENT
 			if (close(previous_pipe_read) == -1) {
-				fprintf(stderr, "error closing unused readable pipe");
+				fprintf(stderr, "error closing unused readable pipe\n");
 				exit(1);
 			}
-			/*if (close(pipefd[1]) == -1) {
-				fprintf(stderr, "error closing unused writeable pipe");
-				exit(1);
-			}*/
 			wait(&exit_status);
 			return WEXITSTATUS(exit_status);
 	}
