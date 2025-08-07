caf69689433f78251764721d71a48c949be568bb
Evan Aceves <evanm.aceves@gmail.com>
2025-08-06 18:18:05 -0700

    Lab 4: Sped up v2 by adding lock only where needed

--- Lab_4/hash-table-v2.c
+++ Lab_4/hash-table-v2.c
@@ -75,7 +75,6 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              const char *key,
                              uint32_t value)
 {
-	pthread_mutex_lock(&v2_lock); /* Start critical code */
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -89,6 +88,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	list_entry = calloc(1, sizeof(struct list_entry));
 	list_entry->key = key;
 	list_entry->value = value;
+	pthread_mutex_lock(&v2_lock); /* Start critical code */
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 	pthread_mutex_unlock(&v2_lock); /* End critical code */
 }
